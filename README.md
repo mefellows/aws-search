313e6eeb9b1bb16f3623a09464a7770e283ae2b3
Matt Fellows <matt.fellows@onegeek.com.au>
2015-10-07 11:13:48 +1100

    Added beanstalk and other EC2 queries. JSON formatted responses

--- README.md
+++ README.md
@@ -9,8 +9,23 @@ Simple AWS cross-account search tool, leveraging Credulous
 ## Usage
 
 ```
-aws-search --action ami --id ami-12345678
-aws-search --action instance-id --id i-12345678
+aws-search --id ami-5678bbb --action ami --region ap-southeast-2
+aws-search --id i-1234aaaa --action instance-id --region ap-southeast-2
+aws-search --id 54.34.3.1 --action public-ip --region ap-southeast-2.
+aws-search --id myapp --action eb --region ap-southeast-2
+aws-search --id production-a-1234aaa --action eb-env --region ap-southeast-2
+aws-search --id production-a-123aaa --action eb-resources --region ap-southeast-2
+```
+
+For verbose logging, simply add the `--verbose` flag.
+
+## Unix philosophy
+
+AWS Search is desgined to be combined with other tools, for example, it goes realli well 
+with [jq](https://github.com/stedolan/jq) too:
+
+```
+./aws-search --id production-a-123aaa  --action eb-resources --region ap-southeast-2 | jq .EnvironmentResources.Instances[0].Id
 ```
 
 NOTE: `AWS_REGION` environment variable will be used if no `-r` option is present.
--- main.go
+++ main.go
@@ -3,11 +3,15 @@ package main
 // Simple AMI query tool: uses basic loops
 
 import (
+	"encoding/json"
 	"flag"
+	"fmt"
 	aws "github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/service/ec2"
+	eb "github.com/aws/aws-sdk-go/service/elasticbeanstalk"
 	"github.com/mefellows/credulous/credulous"
+	"io/ioutil"
 	"log"
 	"os"
 	"strings"
@@ -17,9 +21,10 @@ import (
 func main() {
 
 	type config struct {
-		region string
-		id     string
-		action string
+		region  string
+		id      string
+		action  string
+		verbose bool
 	}
 
 	// Get arguments
@@ -29,11 +34,16 @@ func main() {
 	flag.StringVar(&c.region, "r", os.Getenv("AWS_REGION"), "Region")
 	flag.StringVar(&c.id, "id", "", "Resource ID to find")
 	flag.StringVar(&c.action, "action", "instance", "AWS resource (one of [instance, ip, ami])")
+	flag.BoolVar(&c.verbose, "verbose", false, "Verbose output. Warning: This may disrupt output/pipe processing")
 	flag.Parse()
 
 	if c.region == "" || c.id == "" || c.action == "" {
 		flag.Usage()
 		os.Exit(1)
+	}
+
+	if !c.verbose {
+		log.SetOutput(ioutil.Discard)
 	}
 
 	accts := credulous.GetAccounts()
@@ -51,20 +61,37 @@ func main() {
 	// Loop through all of the accounts, search for instance in parallel
 	for key, value := range creds {
 		go func(key string, value string) {
-			svc := ec2.New(&aws.Config{
+			config := &aws.Config{
 				Region:      aws.String(c.region),
 				Credentials: credentials.NewStaticCredentials(key, value, ""),
-			})
+			}
+			svc := ec2.New(config)
+			ebSvc := eb.New(config)
 
+			var r interface{}
 			switch strings.ToLower(c.action) {
 			case "instance-id":
-				queryInstance(svc, c.id)
+				r = queryInstance(svc, "instance-id", c.id)
 			case "ami":
-				queryAmi(svc, c.id)
+				r = queryAmi(svc, c.id)
 			case "ip":
+				r = queryInstance(svc, "private-ip-address", c.id)
+			case "public-ip":
+				r = queryInstance(svc, "ip-address", c.id)
+			case "eb":
+				r = queryBeanstalk(ebSvc, c.id)
+			case "eb-resources":
+				r = queryBeanstalkResources(ebSvc, c.id)
+			case "eb-env":
+				r = queryBeanstalkEnv(ebSvc, c.id)
 			default:
 				log.Fatalf("Action '%s' is not a valid action", c.action)
+			}
 
+			if r != nil {
+				v, err := json.Marshal(r)
+				checkError(err)
+				fmt.Printf("%s", v)
 			}
 			done.Done()
 		}(key, value)
@@ -73,7 +100,7 @@ func main() {
 }
 
 // Return true if AMI exists
-func queryAmi(service *ec2.EC2, ami string) bool {
+func queryAmi(service *ec2.EC2, ami string) interface{} {
 	input := ec2.DescribeImagesInput{
 		ImageIds: []*string{&ami},
 	}
@@ -83,22 +110,70 @@ func queryAmi(service *ec2.EC2, ami string) bool {
 		image := output.Images[0]
 		log.Printf("Found image in account (%s): %s, with name: %s\n", *image.OwnerId, *image.Name)
 		log.Printf("Tags: %v", image.Tags)
-		return true
+		return image
 	}
-	return false
+	return nil
 }
-func queryInstance(service *ec2.EC2, id string) bool {
+
+func queryBeanstalk(svc *eb.ElasticBeanstalk, filterVal string) interface{} {
+	params := &eb.DescribeApplicationsInput{
+		ApplicationNames: []*string{
+			aws.String(filterVal),
+		},
+	}
+	resp, err := svc.DescribeApplications(params)
+	checkError(err)
+	if len(resp.Applications) > 0 {
+		return resp
+	}
+
+	return nil
+}
+
+func queryBeanstalkResources(svc *eb.ElasticBeanstalk, filterVal string) interface{} {
+	params := &eb.DescribeEnvironmentResourcesInput{
+		EnvironmentName: aws.String(filterVal),
+	}
+	resp, err := svc.DescribeEnvironmentResources(params)
+	checkError(err)
+
+	if resp.EnvironmentResources != nil {
+		return resp
+	}
+
+	return nil
+}
+
+func queryBeanstalkEnv(svc *eb.ElasticBeanstalk, filterVal string) interface{} {
+	params := &eb.DescribeEnvironmentsInput{
+		EnvironmentNames: []*string{aws.String(filterVal)},
+	}
+	resp, err := svc.DescribeEnvironments(params)
+	checkError(err)
+	if len(resp.Environments) > 0 {
+		return resp
+	}
+
+	return nil
+}
+
+func queryInstance(service *ec2.EC2, filter string, filterVal string) interface{} {
 	params := &ec2.DescribeInstancesInput{
-		MaxResults: aws.Int64(1),
-		InstanceIds: []*string{
-			aws.String(id),
+		Filters: []*ec2.Filter{
+			{
+				Name: aws.String(filter),
+				Values: []*string{
+					aws.String(filterVal),
+				},
+			},
 		},
 	}
 	resp, err := service.DescribeInstances(params)
-	checkError(err)
-	log.Printf("Response: %v\n", resp)
-
-	return true
+	if len(resp.Reservations) > 0 {
+		checkError(err)
+		return resp
+	}
+	return nil
 }
 
 func checkError(err error) {
